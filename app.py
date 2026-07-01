cca30dba06d300c83bc768a10e14eab6587b3c7c
Malachi Davey <malachilddavey@gmail.com>
2026-07-01 16:57:09 +0000

    added register

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 import git
 from flask import Flask, render_template, url_for, flash, redirect, request
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'abc123'
 app.debug = True
 toolbar = DebugToolbarExtension(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  password = db.Column(db.String(60), nullable=False)
+
+  def __repr__(self):
+    return f"User('{self.username}', '{self.email}')"
+
+with app.app_context():
+  db.create_all()
+
 
 @app.route('/')
 def index():
@@ -20,6 +37,16 @@ def webhook():
         return 'Updated PythonAnywhere successfully', 200
     else:
         return 'Wrong event type', 400
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit(): #already in your code file
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Account created for {form.username.data}!', 'success')
+
 
 if __name__ == '__main__':
     app.run()
