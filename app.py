7168d12efaf11503f386fef2d1d8d72b600fe8a8
Malachi Davey <malachilddavey@gmail.com>
2026-07-01 16:38:28 +0000

    fixed app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import git
-from flask import Flask, render_template, url_for, flash, redirect, requestfrom flask_debugtoolbar 
-import DebugToolbarExtension
+from flask import Flask, render_template, url_for, flash, redirect, request
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'abc123'
