42b195221ccc280f6e97f70588fc7244bd32c3dd
Malachi Davey <malachilddavey@gmail.com>
2026-07-01 16:31:16 +0000

    add webhook

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
-from flask_debugtoolbar import DebugToolbarExtension
+import git
+from flask import Flask, render_template, url_for, flash, redirect, requestfrom flask_debugtoolbar 
+import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'abc123'
@@ -9,6 +10,16 @@ toolbar = DebugToolbarExtension(app)
 @app.route('/')
 def index():
     return "Hello, Flask!"
+
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/CHANGE_TO_PYTHON_ANYWHERE_USERNAME/CHANGE_TO_GITHUB_REPO_NAME')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 if __name__ == '__main__':
     app.run()
